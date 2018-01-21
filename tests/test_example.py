88c44817467e043a529b85585f63722aa8fab3d7
Bruno Alla <bruno.alla@founders4schools.org.uk>
2018-01-21 19:03:35 +0000

    Make basic test a bit more relevant

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,2 +1,6 @@
-def test_dummy():
-    assert 1 + 1 == 2
+def test_run_basic(cookies):
+    result = cookies.bake(extra_context={'project_slug': 'lambda_func'})
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project.basename == 'lambda_func'
+    assert result.project.isdir()
