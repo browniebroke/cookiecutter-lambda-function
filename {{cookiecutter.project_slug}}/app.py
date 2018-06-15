19ebd354258be6b01f1e4d49d583195938cb139c
Bruno Alla <bruno.alla@founders4schools.org.uk>
2018-06-15 19:38:28 +0100

    Tweak Jinja2 tags to make code more readable

--- {{cookiecutter.project_slug}}/app.py
+++ {{cookiecutter.project_slug}}/app.py
@@ -1,13 +1,17 @@
 from chalice import Chalice
 
-app = Chalice(app_name='{{ cookiecutter.project_slug }}'){% if cookiecutter.endpoint == "y" %}
+app = Chalice(app_name='{{ cookiecutter.project_slug }}')
+{%- if cookiecutter.endpoint == "y" %}
 
 
 @app.route('/')
 def index():
-    return {'hello': 'world'}{% endif %}{% if cookiecutter.schedule == "y" %}
+    return {'hello': 'world'}
+{%- endif %}
+{%- if cookiecutter.schedule == "y" %}
 
 
 @app.schedule('rate(1 hour)')
 def every_hour(event):
-    print(event.to_dict()){% endif %}
+    print(event.to_dict())
+{%- endif %}
