b39db4bc8999e35aa88a9df5c8488603a6ac4308
Bruno Alla <browniebroke@users.noreply.github.com>
2018-07-10 11:53:04 +0100

    Handle existing vendored dependency

--- {{cookiecutter.project_slug}}/bin/vendor_package.py
+++ {{cookiecutter.project_slug}}/bin/vendor_package.py
@@ -10,4 +10,7 @@ for package_name in sys.argv[1:]:
     package = import_module(package_name)
     package_path = package.__path__[0]
     vendor_path = (Path(__file__) / ".." / ".." / "vendor").resolve()
-    shutil.copytree(package_path, vendor_path / package_name)
+    vendor_package = vendor_path / package_name
+    if vendor_package.exists():
+        shutil.rmtree(vendor_package)
+    shutil.copytree(package_path, vendor_package)
